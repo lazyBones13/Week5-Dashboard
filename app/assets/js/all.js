68c75e9466c9e1af679296024b7da2e0bc504186
JM <s9923024@gmail.com>
2021-08-05 00:32:54 +0800

    新增第一版assignment

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,5 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+$('.list-group-item').click(function (e) {
+  e.preventDefault()
+  // 請使用 toggleClass 完成此作業
+  $(this).toggleClass('active')
+})
