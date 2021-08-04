3117cfb71cdf52fd4ae02baff183a959d8369e1f
JM <s9923024@gmail.com>
2021-08-05 00:36:22 +0800

    Update 2021-08-04T16:36:01.179Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-$('.list-group-item').click(function (e) {
-  e.preventDefault()
-  // 請使用 toggleClass 完成此作業
-  $(this).toggleClass('active')
-})
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$('.list-group-item').click(function (e) {
+  e.preventDefault(); // 請使用 toggleClass 完成此作業
+
+  $(this).toggleClass('active');
+});
+//# sourceMappingURL=all.js.map
