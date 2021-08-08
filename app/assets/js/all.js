bfabe7093d0e783061ceab264be25fcd82b02664
JM <s9923024@gmail.com>
2021-08-08 16:35:21 +0800

    增加modal功能

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,18 @@
 $('.list-group-item').click(function (e) {
   e.preventDefault()
-  // 請使用 toggleClass 完成此作業
   $(this).toggleClass('active')
 })
+
+$('.reply-btn').click(function (e) {
+  e.preventDefault()
+  $('.message-reply').addClass('active')
+})
+
+$('.cancel-btn').click(function (e) {
+  e.preventDefault()
+  $('.message-reply').removeClass('active')
+})
+
+// $('.expandMore').click(function (event) {
+//   $('.expandMore').toggleClass('expandLess')
+// })
