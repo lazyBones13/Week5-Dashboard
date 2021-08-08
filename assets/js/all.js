1378d7eea09c55efdadcef26cc649f7835f84273
JM <s9923024@gmail.com>
2021-08-08 16:34:19 +0800

    Update 2021-08-08T08:34:16.852Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,17 @@
 "use strict";
 
 $('.list-group-item').click(function (e) {
-  e.preventDefault(); // 請使用 toggleClass 完成此作業
-
+  e.preventDefault();
   $(this).toggleClass('active');
 });
+$('.reply-btn').click(function (e) {
+  e.preventDefault();
+  $('.message-reply').addClass('active');
+});
+$('.cancel-btn').click(function (e) {
+  e.preventDefault();
+  $('.message-reply').removeClass('active');
+}); // $('.expandMore').click(function (event) {
+//   $('.expandMore').toggleClass('expandLess')
+// })
 //# sourceMappingURL=all.js.map
